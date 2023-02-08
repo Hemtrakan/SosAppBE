611212000c9367c57d852a5296f678ab5abefcb3
Chirapon Hemtrakan <Chirapon.job@gmail.com>
2023-02-08 18:51:20 +0700

    add messages service

---
+++ messages/constant/constant.go
@@ -0,0 +1,20 @@
+package constant
+
+const (
+	SuccessMsg  string = "Success"
+	SuccessCode string = "0"
+	ErrorCode   string = "400"
+)
+
+const (
+	Required string = "required"
+	Min      string = "min"
+	Max      string = "max"
+	Length   string = "len"
+	Numeric  string = "numeric"
+)
+
+const (
+	User  string = "user"
+	Admin string = "admin"
+)
