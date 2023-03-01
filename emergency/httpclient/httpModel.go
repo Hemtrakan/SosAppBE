e666205f91975e0f8cf1efb2deebc0da52845773
Chirapon Hemtrakan <chirapon.job@gmail.com>
2023-03-01 14:39:46 +0700

    update structure project

--- emergency/httpclient/httpModel.go
+++ emergency/httpclient/httpModel.go
@@ -1,1 +1,25 @@
 package httpclient
+
+import (
+	"net/http"
+	"time"
+)
+
+type HttpClient struct {
+	MaxConnections  int
+	Timeout         time.Duration
+	CertSkipVerify  bool
+	CertServerName  string
+	CertPEMFileName string
+	Charset         string
+	ProxyURL        string
+}
+
+type HttpResponse struct {
+	HttpStatusCode int
+	HttpStatusMsg  string
+	ResponseMsg    string
+	HttpHeader     http.Header
+	IsRedirect     bool
+	RedirectUrl    string
+}
--- emergency/restapi/main.go
+++ emergency/restapi/main.go
@@ -1,11 +1,17 @@
 package restapi
 
 import (
+	"emergency/constant"
+	"emergency/utility/response"
+	"emergency/utility/token"
+	"github.com/golang-jwt/jwt/v4"
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	config "github.com/spf13/viper"
 	"github.com/tylerb/graceful"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -16,17 +22,70 @@ func NewControllerMain(ctrl Controller) {
 	e.Use(middleware.Recover())
 	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(200)))
 
-	//r := e.Group("/SosApp")
+	configs := echojwt.Config{
+		ErrorHandler:  ErrorHandler,
+		SigningKey:    []byte(config.GetString("jwt.secret")),
+		NewClaimsFunc: NewClaimsFunc,
+	}
+
 	r := e.Group(config.GetString("service.endpoint"))
+
 	r.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, "Ok Service Emergency")
 	})
 
+	a := r.Group(config.GetString("role.admin"))
+	a.Use(echojwt.WithConfig(configs), AuthRoleAdmin)
+
+	//r := e.Group("/SosApp")
+
+	//e.Logger.Fatal(e.Start(":80"))
 	e.Start(":" + config.GetString("service.port"))
 	//e.Server.Addr = ":" + config.GetString("service.port")
 	err := graceful.ListenAndServe(e.Server, 5*time.Second)
 
 	if err != nil {
 		panic(err)
+	}
+}
+
+func NewClaimsFunc(c echo.Context) jwt.Claims {
+	return new(token.JwtCustomClaims)
+}
+
+func ErrorHandler(c echo.Context, e error) error {
+	var res response.RespMag
+	res.Code = constant.ErrorStatusUnauthorized
+	res.Msg = "Unauthorized"
+	return c.JSON(http.StatusUnauthorized, res)
+}
+
+func AuthRoleUser(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		user := c.Get("user").(*jwt.Token)
+		claims := user.Claims.(*token.JwtCustomClaims)
+		var userRole = claims.Role
+		role := strings.Replace(config.GetString("role.user"), "/", "", 2)
+		if userRole == role {
+			return next(c)
+		} else {
+			c.Error(echo.ErrUnauthorized)
+			return nil
+		}
+	}
+}
+
+func AuthRoleAdmin(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		user := c.Get("user").(*jwt.Token)
+		claims := user.Claims.(*token.JwtCustomClaims)
+		var userRole = claims.Role
+		role := strings.Replace(config.GetString("role.admin"), "/", "", 2)
+		if userRole == role {
+			return next(c)
+		} else {
+			c.Error(echo.ErrUnauthorized)
+			return nil
+		}
 	}
 }
