72ac02ba2a20e9a04296a699b5b1262bc5e4baf2
Chirapon Hemtrakan <chirapon.job@gmail.com>
2023-04-25 20:43:55 +0700

    updata service socket.io nodejs

--- socket/app.js
+++ socket/app.js
@@ -1,26 +1,45 @@
-const express = require('express');
+const express = require("express");
+const router = express.Router();
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const messages = []
+const messages = [];
 
+let count = 0;
 
-io.on('connection', (socket) => {
-  const username = socket.handshake.query.username
-  socket.on('message', (data) => {
+io.of(/^\/\d+$/).on("connection", (socket) => {
+  count++;
+  console.log(count);
+  const newNamespace = socket.nsp; 
+  let roomChatId = socket.nsp.name.replace("/", '');
+  socket.on("disconnect", () => {
+    socket.disconnect();
+  });
+
+  const username = socket.handshake.query.username;
+  console.log("----- Connect --------");
+  console.log("username : ", username);
+  console.log("----- Connect --------");
+  socket.on(roomChatId, (data) => {
     const message = {
       message: data.message,
       senderUsername: username,
-      sentAt: Date.now()
-    }
-    messages.push(message)
-    io.emit('message', message)
-
-  })
+      sentAt: Date.now(),
+    };
+    messages.push(message);
+    console.log("message : ", message);
+    console.log("roomChatId :",roomChatId);
+    newNamespace.emit(roomChatId, message);
+  });
 });
+
+
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
+
+
+
